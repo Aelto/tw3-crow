649abccdd1402213544b131545e8172ed800647a
Aelto <Aelto@users.noreply.github.com>
2025-08-01 17:34:43 +0200

    release script: add bundled version, unify folder hierarchy

--- scripts/release.bat
+++ scripts/release.bat
@@ -1,55 +1,53 @@
 call variables.cmd
 
+set releasemods=%modpath%\release\mods
+set bundleddir=%modpath%\release.bundled
+set bundledmod=%bundleddir%\mods\modCROW
+set localpath=content\scripts\local\crow
+set bundledout=%bundledmod%\%localpath%
+
 rmdir "%modpath%\release" /s /q
-mkdir "%modpath%\release"
+rmdir "%bundleddir%\" /s /q
+
+mkdir "%releasemods%"
+mkdir "%bundledout%"
 
 :: here we compile the cahirc scripts, then construct the final mods by combining
 :: the vanilla edits of the mods and their local scripts:
 call compile
 
-set modname=modCrowCombatStamina
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\local\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
+call :movetorelease modCrowCommon true
+call :movetorelease modCrowCombatStamina true
+call :movetorelease modCrowCombatDefense true
+call :movetorelease modCrowCombatAggression true
+call :movetorelease modCrowCombatEffects true
+:: this one is not included in the bundled version as it is still a wip
+call :movetorelease modCrowCombatRegeneration false
 
-tw3-cahirp build --game "%gamePath%" --recipes "%modpath%/release/mods/%modname%/cahirp" --out "%modpath%/release/mods/%modname%/content/scripts" --without-mods
+call compileblob
 
-set modname=modCrowCombatDefense
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\local\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::FUNCTIONS::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+goto:eof
 
-tw3-cahirp build --game "%gamePath%" --recipes "%modpath%/release/mods/%modname%/cahirp" --out "%modpath%/release/mods/%modname%/content/scripts" --without-mods
+:: moves the provided module to the normal release
+::
+:: the second parameters defines whether it should also be added to the bundled
+:: release, which is a single mod folder with all local files in the same place
+:movetorelease
+  move "%modpath%\dist\%~1" "%releasemods%\"
 
-set modname=modCrowCombatAggression
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\local\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-
-set modname=modCrowCombatRegeneration
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\crow\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-
-set modname=modCrowCombatEffects
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\crow\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
+  set shouldmovetobundled=%~2
+  if "%shouldmovetobundled%" == "true" (
+    call :movetobundled %~1
+  )
+goto:eof
 
 
-set modname=modCrowCommon
-XCOPY "%modpath%\src\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-rmdir "%modpath%\release\mods\%modname%\content\scripts\local\" /s /q
-XCOPY "%modpath%\dist\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
+:: moves the provided module to the bundled release
+:movetobundled
+  echo Moving %~1 to bundled release
 
-:: move the strings
-set modname=modCrowCommon
-XCOPY "%modpath%\strings" "%modpath%\release\mods\%modName%\content\" /e /s /y
-
-:: copy the sharedutils dependencies
-set modname=mod_sharedutils_helpers
-XCOPY "%modpath%\tw3-shared-utils\%modname%\" "%modpath%\release\mods\%modname%\" /e /s /y
-
-:: don't need a menu at the moment
-@REM mkdir "%modpath%\release\bin\config\r4game\user_config_matrix\pc\"
-@REM copy "%modpath%\mod-menu.xml" "%modpath%\release\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
+  XCOPY "%releasemods%\%~1\%localpath%\" "%bundledout%"  /e /s /y
+goto:eof
